9044638daf5916660266847a441400c4e65dc37d
Sachin <sachi77n@gmail.com>
2014-07-21 23:24:36 +0530

    Fixed issue doesnt work on ios! #51..

--- light-gallery/js/lightGallery.js
+++ light-gallery/js/lightGallery.js
@@ -1,8 +1,8 @@
 /** ==========================================================
 
-* jquery lightGallery.js v1.1.0
+* jquery lightGallery.js v1.1.2
 * http://sachinchoolur.github.io/lightGallery/
-* Released under the Apache License - http://opensource.org/licenses/Apache-2.0
+* Released under the Apache License - http://opensource.org/licenses/Apache-2.0  ---- FREE ----
 
 =========================================================/**/
 ;
@@ -12,7 +12,7 @@
         var defaults = {
             mode: 'slide',
             useCSS: true,
-            easing: 'linear', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+            easing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
             speed: 1000,
             closable: true,
             loop: false,
@@ -60,7 +60,6 @@
             $gallery, $galleryCont, $slider, $slide, $prev, $next, prevIndex, $thumb_cont, $thumb, windowWidth, interval, usingThumb = false,
             aTiming = false,
             aSpeed = false;
-            var isiPad = navigator.userAgent.match(/iPad/i) != null;
         var settings = $.extend(true, {}, defaults, options);
         var lightGallery = {
             init: function () {
@@ -588,11 +587,12 @@
                     settings.onSlideBefore.call(this);
                 }
                 if (settings.mode === 'slide') {
-                   // if (this.doCss() && !$slider.hasClass('slide') && !isiPad) {
-                     //   $slider.addClass('slide');
-                  //  }else if(this.doCss() && !$slider.hasClass('useLeft') &&isiPad){
+                    var isiPad = navigator.userAgent.match(/iPad/i) != null;
+                    if (this.doCss() && !$slider.hasClass('slide') && !isiPad) {
+                        $slider.addClass('slide');
+                    }else if(this.doCss() && !$slider.hasClass('useLeft') &&isiPad){
                         $slider.addClass('useLeft');
-                   // }
+                    }
                     /*                  if(this.doCss()){
                         $slider.css({ 'transform' : 'translate3d('+(-index*100)+'%, 0px, 0px)' });
                     }*/
@@ -640,10 +640,14 @@
                     $thumb.removeClass('active');
                     $thumb.eq(index).addClass('active');
                 }
-                if (settings.controls && settings.hideControlOnEnd && settings.loop === false) {
+                if (settings.controls && settings.hideControlOnEnd && settings.loop === false && $children.length > 1) {
+                    var l = $children.length;
+                        l = parseInt(l)-1;
                     if (index === 0) {
                         $prev.addClass('disabled');
-                    } else if (index === $children.length - 1) {
+                        $next.removeClass('disabled');
+                    } else if (index === l) {
+                        $prev.removeClass('disabled');
                         $next.addClass('disabled');
                     } else {
                         $prev.add($next).removeClass('disabled');
