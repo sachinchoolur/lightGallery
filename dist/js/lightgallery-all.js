e97f12fd835712277ea50d31122a8f1069cf9d55
Matias Sebastiao <msebastiao@coderik.com>
2018-01-20 16:29:24 -0300

    Compiling last change downloadName

--- dist/js/lightgallery-all.js
+++ dist/js/lightgallery-all.js
@@ -1,16 +1,16 @@
-/*! lightgallery - v1.6.6 - 2017-12-20
+/*! lightgallery - v1.6.6 - 2018-01-20
 * http://sachinchoolur.github.io/lightGallery/
-* Copyright (c) 2017 Sachin N; Licensed GPLv3 */
-/*! lightgallery - v1.6.6 - 2017-12-20
+* Copyright (c) 2018 Sachin N; Licensed GPLv3 */
+/*! lightgallery - v1.6.6 - 2018-01-20
 * http://sachinchoolur.github.io/lightGallery/
-* Copyright (c) 2017 Sachin N; Licensed GPLv3 */
+* Copyright (c) 2018 Sachin N; Licensed GPLv3 */
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define(['jquery'], function (a0) {
       return (factory(a0));
     });
-  } else if (typeof exports === 'object') {
+  } else if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
@@ -801,6 +801,16 @@
 
                 if (_src) {
                     $('#lg-download').attr('href', _src);
+                    
+                    var _downloadName;
+                    if (_this.s.dynamic) {
+                        _downloadName = _this.s.dynamicEl[index].downloadName || '';
+                    } else {
+                        _downloadName = _this.$items.eq(index).attr('data-download-name');	
+                    }                    
+                    if (_downloadName) {
+                        $('#lg-download').attr('download', _downloadName);
+                    }                    
                     _this.$outer.removeClass('lg-hide-download');
                 } else {
                     _this.$outer.addClass('lg-hide-download');
