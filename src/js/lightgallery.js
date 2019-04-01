bccd69788c03a389b03cf2f80120afe885086ceb
Mike <miguel@guumo.com>
2019-04-01 21:21:14 +0200

    Fix  misspelling

--- src/js/lightgallery.js
+++ src/js/lightgallery.js
@@ -420,7 +420,7 @@
                     html5: true
                 };
             } else {
-                console.error('lightGallery :- data-src is not pvovided on slide item ' + (index + 1) + '. Please make sure the selector property is properly configured. More info - http://sachinchoolur.github.io/lightGallery/demos/html-markup.html');
+                console.error('lightGallery :- data-src is not provided on slide item ' + (index + 1) + '. Please make sure the selector property is properly configured. More info - http://sachinchoolur.github.io/lightGallery/demos/html-markup.html');
                 return false;
             }
         }
