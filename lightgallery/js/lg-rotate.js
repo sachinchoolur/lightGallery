4474851b2a13acbe57b3e97c2343b09896a1a05e
Sachin Neravath <sachi77n@gmail.com>
2020-09-01 16:17:27 +0530

    Update rotate files

--- lightgallery/js/lg-rotate.js
+++ lightgallery/js/lg-rotate.js
@@ -1,23 +1,23 @@
-/*! lg-rotate - v1.0.0 - 2020-08-29
+/*! lg-rotate - v1.1.0 - 2020-09-01
 * http://sachinchoolur.github.io/lightGallery
 * Copyright (c) 2020 Sachin N; Licensed GPLv3 */
 
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module unless amdModuleId is set
-    define(['jquery'], function (a0) {
-      return (factory(a0));
-    });
-  } else if (typeof module === 'object' && module.exports) {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
-    module.exports = factory(require('jquery'));
-  } else {
-    factory(root["jQuery"]);
-  }
-}(this, function ($) {
-
+    if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module unless amdModuleId is set
+      define(['jquery'], function (a0) {
+        return (factory(a0));
+      });
+    } else if (typeof module === 'object' && module.exports) {
+      // Node. Does not work with strict CommonJS, but
+      // only CommonJS-like environments that support module.exports,
+      // like Node.
+      module.exports = factory(require('jquery'));
+    } else {
+      factory(root["jQuery"]);
+    }
+  }(this, function ($) {
+  
 (function () {
     'use strict';
 
@@ -61,7 +61,11 @@
     Rotate.prototype.init = function () {
         var _this = this;
         this.buildTemplates();
-        this.resetRotateValues();
+
+        // Save rotate config for each item to persist its rotate, flip values
+        // even after navigating to diferent slides
+        this.rotateValuesList = {};
+
 
         // event triggered after appending slide content
         this.core.$el.on('onAferAppendSlide.lg.tm.rotate', function (event, index) {
@@ -86,8 +90,14 @@
             .on('click.lg', this.flipVertical.bind(this));
 
         // Reset rotate on slide change
-        this.core.$el.on('onBeforeSlide.lg.tm.rotate', function () {
-            _this.resetRotateValues();
+        this.core.$el.on('onBeforeSlide.lg.tm.rotate', function (event, prevIndex, index) {
+            if (!_this.rotateValuesList[index]) {
+                _this.rotateValuesList[index] = {
+                    rotate: 0,
+                    flipHorizontal: 1,
+                    flipVertical: 1,
+                };
+            }
         });
     };
 
@@ -95,43 +105,36 @@
         var $image = this.core.$slide.eq(this.core.index).find('.lg-img-rotate');
         $image.css(
             'transform',
-            'rotate(' + this.rotateValues.rotate + 'deg) scale3d(' + this.rotateValues.flipVertical + ', ' + this.rotateValues.flipHorizontal + ', 1)'
+            'rotate(' + this.rotateValuesList[this.core.index].rotate + 'deg)' +
+            ' scale3d(' + this.rotateValuesList[this.core.index].flipVertical +
+            ', ' + this.rotateValuesList[this.core.index].flipHorizontal + ', 1)'
         );
     };
 
     Rotate.prototype.rotateLeft = function () {
-        this.rotateValues.rotate -= 90;
+        this.rotateValuesList[this.core.index].rotate -= 90;
         this.applyStyles();
     };
 
     Rotate.prototype.rotateRight = function () {
-        this.rotateValues.rotate += 90;
+        this.rotateValuesList[this.core.index].rotate += 90;
         this.applyStyles();
     };
 
     Rotate.prototype.flipHorizontal = function () {
-        this.rotateValues.flipHorizontal *= -1;
+        this.rotateValuesList[this.core.index].flipHorizontal *= -1;
         this.applyStyles();
     };
 
     Rotate.prototype.flipVertical = function () {
-        this.rotateValues.flipVertical *= -1;
+        this.rotateValuesList[this.core.index].flipVertical *= -1;
         this.applyStyles();
-    };
-
-    // Reset rotate effect
-    Rotate.prototype.resetRotateValues = function () {
-        this.rotateValues = {
-            rotate: 0,
-            flipHorizontal: 1,
-            flipVertical: 1,
-        };
     };
 
     Rotate.prototype.destroy = function () {
         // Unbind all events added by lightGallery rotate plugin
         this.core.$el.off('.lg.tm.rotate');
-        this.resetRotateValues();
+        this.rotateValuesList = {};
     };
 
     $.fn.lightGallery.modules.rotate = Rotate;
@@ -139,3 +142,4 @@
 
 
 }));
+  
