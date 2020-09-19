7ba3ad5a8171a0aae5901f7ceef5d6d808e9f9e1
Tim Gates <tim.gates@iress.com>
2020-09-20 06:25:33 +1000

    docs: Fix simple typo, exicute -> execute
    There is a small typo in src/js/lightgallery.js.
    
    Should read `execute` rather than `exicute`.

--- src/js/lightgallery.js
+++ src/js/lightgallery.js
@@ -44,7 +44,7 @@
 
         /**
          * @desc number of preload slides
-         * will exicute only after the current slide is fully loaded.
+         * will execute only after the current slide is fully loaded.
          *
          * @ex you clicked on 4th image and if preload = 1 then 3rd slide and 5th
          * slide will be loaded in the background after the 4th slide is fully loaded..
