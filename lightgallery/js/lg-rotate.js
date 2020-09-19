1ebba9936be22e7ee0bc9f7cc22dc841b5b329f2
Sachin Neravath <sachi77n@gmail.com>
2020-09-19 17:38:41 +0530

    Update rotate module files

--- lightgallery/js/lg-rotate.js
+++ lightgallery/js/lg-rotate.js
@@ -1,23 +1,23 @@
-/*! lg-rotate - v1.1.0 - 2020-09-01
+/*! lg-rotate - v1.2.0 - 2020-09-19
 * http://sachinchoolur.github.io/lightGallery
 * Copyright (c) 2020 Sachin N; Licensed GPLv3 */
 
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-      // AMD. Register as an anonymous module unless amdModuleId is set
-      define(['jquery'], function (a0) {
-        return (factory(a0));
-      });
-    } else if (typeof module === 'object' && module.exports) {
-      // Node. Does not work with strict CommonJS, but
-      // only CommonJS-like environments that support module.exports,
-      // like Node.
-      module.exports = factory(require('jquery'));
-    } else {
-      factory(root["jQuery"]);
-    }
-  }(this, function ($) {
-  
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(['jquery'], function (a0) {
+      return (factory(a0));
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require('jquery'));
+  } else {
+    factory(root["jQuery"]);
+  }
+}(this, function ($) {
+
 (function () {
     'use strict';
 
@@ -43,11 +43,11 @@
 
     Rotate.prototype.buildTemplates = function () {
         var rotateIcons = '';
-        if (this.core.s.flipHorizontal) {
-            rotateIcons += '<button aria-label="Flip horizontal" class="lg-flip-hor lg-icon"></button>';
-        }
         if (this.core.s.flipVertical) {
-            rotateIcons += '<button aria-label="flip vertical" class="lg-flip-ver lg-icon"></button>';
+            rotateIcons += '<button aria-label="Flip vertical" class="lg-flip-ver lg-icon"></button>';
+        }
+        if (this.core.s.flipHorizontal) {
+            rotateIcons += '<button aria-label="flip horizontal" class="lg-flip-hor lg-icon"></button>';
         }
         if (this.core.s.rotateLeft) {
             rotateIcons += '<button aria-label="Rotate left" class="lg-rotate-left lg-icon"></button>';
@@ -106,8 +106,8 @@
         $image.css(
             'transform',
             'rotate(' + this.rotateValuesList[this.core.index].rotate + 'deg)' +
-            ' scale3d(' + this.rotateValuesList[this.core.index].flipVertical +
-            ', ' + this.rotateValuesList[this.core.index].flipHorizontal + ', 1)'
+            ' scale3d(' + this.rotateValuesList[this.core.index].flipHorizontal +
+            ', ' + this.rotateValuesList[this.core.index].flipVertical + ', 1)'
         );
     };
 
@@ -125,7 +125,7 @@
         this.rotateValuesList[this.core.index].flipHorizontal *= -1;
         this.applyStyles();
     };
-
+    
     Rotate.prototype.flipVertical = function () {
         this.rotateValuesList[this.core.index].flipVertical *= -1;
         this.applyStyles();
@@ -142,4 +142,3 @@
 
 
 }));
-  
