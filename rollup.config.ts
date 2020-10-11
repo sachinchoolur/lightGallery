4dc80a535321831c03bd9ea9b870ae9d568b3814
Sachin Neravath <sachi77n@gmail.com>
2020-10-11 18:19:33 +0530

    Add minified output

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,6 +5,7 @@ import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 import license from 'rollup-plugin-license'
+import { uglify } from "rollup-plugin-uglify";
 
 const pkg = require('./package.json')
 
@@ -12,39 +13,76 @@ const path = require('path');
 
 const libraryName = 'lightgallery'
 
-export default {
-  input: `src/${libraryName}.ts`,
-  output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+export default [{
+    input: `src/${libraryName}.ts`,
+    output: [
+        { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+        { file: pkg.module, format: 'es', sourcemap: true },
+    ],
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: [],
+    watch: {
+        include: 'src/**',
+    },
+    plugins: [
+        // Allow json resolution
+        json(),
+        // Compile TypeScript files
+        typescript({ useTsconfigDeclarationDir: true }),
+        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+        commonjs(),
+        // Allow node_modules resolution, so you can use 'external' to control
+        // which external modules to include in the bundle
+        // https://github.com/rollup/rollup-plugin-node-resolve#usage
+        resolve(),
 
-    license({
-      banner: {
-        commentStyle: 'ignored',
-        content: {
-          file: path.join(__dirname, '.banner'),
-        },
-      },
-    }),
+        license({
+            banner: {
+                commentStyle: 'ignored',
+                content: {
+                    file: path.join(__dirname, '.banner'),
+                },
+            },
+        }),
 
-    // Resolve source maps to the original source
-    sourceMaps(),
-  ],
-}
+        // Resolve source maps to the original source
+        sourceMaps(),
+    ],
+},
+{
+    input: `src/${libraryName}.ts`,
+    output: [
+        { file: pkg.min, name: camelCase(libraryName), format: 'umd' },
+    ],
+    sourcemap: false,
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: [],
+    watch: {
+        include: 'src/**',
+    },
+    plugins: [
+        // Allow json resolution
+        json(),
+        // Compile TypeScript files
+        typescript({ useTsconfigDeclarationDir: true }),
+        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+        commonjs(),
+        // Allow node_modules resolution, so you can use 'external' to control
+        // which external modules to include in the bundle
+        // https://github.com/rollup/rollup-plugin-node-resolve#usage
+        resolve(),
+
+        
+        // Resolve source maps to the original source
+        sourceMaps(),
+        uglify(),
+        license({
+            banner: {
+                commentStyle: 'regular',
+                content: {
+                    file: path.join(__dirname, '.banner'),
+                },
+            },
+        }),
+    ],
+}]
