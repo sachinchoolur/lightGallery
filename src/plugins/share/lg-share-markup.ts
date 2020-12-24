90e21b4c38ec8a52837ad0cfa41df246f96a83a7
Sachin Neravath <sachi77n@gmail.com>
2020-12-24 15:40:42 +0530

    accessibility improvements in share plugin

--- src/plugins/share/lg-share-markup.ts
+++ src/plugins/share/lg-share-markup.ts
@@ -1,24 +1,27 @@
 import { lgQuery } from '../../lgQuery';
 
 export default function (selector: lgQuery): void {
-    selector
-        .find('.lg-toolbar')
-        .append(
-            '<div class="lg-share lg-icon"><ul class="lg-dropdown" style="position: absolute;"></ul></div>',
-        );
+    selector.find('.lg-toolbar').append(
+        `<button type="button aria-label="Share" aria-haspopup="true" aria-expanded="false" class="lg-share lg-icon">
+            <ul class="lg-dropdown" style="position: absolute;"></ul></button>`,
+    );
 
     selector.find('.lg').append('<div class="lg-dropdown-overlay"></div>');
-    selector
-        .find('.lg-share')
-        .first()
-        .on('click.lg', () => {
-            selector.toggleClass('lg-dropdown-active');
-        });
+    const $shareButton = selector.find('.lg-share');
+    $shareButton.first().on('click.lg', () => {
+        selector.toggleClass('lg-dropdown-active');
+        if (selector.hasClass('lg-dropdown-active')) {
+            selector.attr('aria-expanded', true);
+        } else {
+            selector.attr('aria-expanded', false);
+        }
+    });
 
     selector
         .find('.lg-dropdown-overlay')
         .first()
         .on('click.lg', () => {
             selector.removeClass('lg-dropdown-active');
+            selector.attr('aria-expanded', false);
         });
 }
