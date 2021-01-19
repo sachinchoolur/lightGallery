22a3199e854cad55bff9434bf8200249c72af1dc
Sachin Neravath <sachi77n@gmail.com>
2021-01-19 15:50:52 +0530

    Add demo page

---
+++ site/assets/js/app.js
@@ -0,0 +1,39 @@
+/**
+ * Inline Gallery
+ */
+const lgCont = document.getElementById('gallery-container');
+lgCont.style.height = window.innerHeight + 'px';
+window.lightGallery(document.getElementById('gallery-container'), {
+    container: document.getElementById('gallery-container'),
+    dynamic: true,
+    thumbnail: true,
+    swipeToClose: false,
+    hash: false,
+    pager: false,
+    closable: false,
+    mobileSettings: {
+        showCloseIcon: false,
+    },
+    dynamicEl: [
+        {
+            src: '/images/demo/image-5.jpg',
+            thumb: '/images/demo/image-1-thumb.jpg',
+        },
+        {
+            src: '/images/demo/image-6.jpg',
+            thumb: '/images/demo/image-2-thumb.jpg',
+        },
+        {
+            src: '/images/demo/image-7.jpg',
+            thumb: '/images/demo/image-3-thumb.jpg',
+        },
+        {
+            src: '/images/demo/image-8.jpg',
+            thumb: '/images/demo/image-4-thumb.jpg',
+        },
+        {
+            src: '/images/demo/image-9.jpg',
+            thumb: '/images/demo/image-5-thumb.jpg',
+        },
+    ],
+});
---
+++ site/content/demos/videos.md
@@ -0,0 +1,26 @@
+---
+title: 'Videos Gallery'
+description: 'LightGallery allows you to build inline image/video galleries.'
+lead:
+    'lightGallery comes with a lot of options, events, and methods to customize
+    the gallery without touching the core code. You can find both lightGallery
+    core options, and the built in plugin options here.'
+date: 2020-10-06T08:48:57+00:00
+lastmod: 2020-10-06T08:48:57+00:00
+draft: false
+images: []
+menu:
+    demos:
+        parent: 'Demos'
+weight: 020
+toc: true
+---
+
+<div class="title-wrap"><h2>Inline Gallery</h2></div>
+    <div id="gallery-container" class="gallery-container">
+</div>
+
+## YouTube video
+
+To add YouTube videos, just copy video url from the youtube addressBar and pass
+it through data-src attribute
