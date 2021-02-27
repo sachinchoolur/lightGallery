905fde4038319b0845261bfd767b89109ae7d9dd
Sachin Neravath <sachi77n@gmail.com>
2021-02-27 13:53:19 +0530

    Add generateUUID method

--- src/lgQuery.ts
+++ src/lgQuery.ts
@@ -29,6 +29,17 @@ interface Offset {
 })();
 export class lgQuery {
     static eventListeners: { [key: string]: any[] } = {};
+    static generateUUID(): string {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+            /[xy]/g,
+            function (c) {
+                const r = (Math.random() * 16) | 0,
+                    v = c == 'x' ? r : (r & 0x3) | 0x8;
+                return v.toString(16);
+            },
+        );
+    }
+
     private selector: any;
     private firstElement: any;
     private cssVenderPrefixes: string[] = [
