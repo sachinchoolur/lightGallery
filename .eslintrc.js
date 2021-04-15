ec8548389c5d0035c1229e21682ab427d6217bfd
Sachin Neravath <sachi77n@gmail.com>
2021-04-15 15:54:45 +0530

    chore(eslintrc.js): disable no-empty-interface rule
    Empty interfaces are required to generate events docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
     rules: {
+        '@typescript-eslint/no-empty-interface': 'off',
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     },
