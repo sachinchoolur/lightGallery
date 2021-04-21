d14e5bd98328dc9f26741bf4fce6d0a0dd0370f0
Sachin Neravath <sachi77n@gmail.com>
2021-04-21 08:16:13 +0530

    docs(events): update events demo with updated event names

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -303,7 +303,7 @@ const galleryEventsDemo = $galleryEventsDemo[0];
 
 if (galleryEventsDemo) {
     const colours = ['#6a7583', '#1e304b', '#315460', '#080607'];
-    galleryEventsDemo.addEventListener('beforeSlide', (event) => {
+    galleryEventsDemo.addEventListener('lgBeforeSlide', (event) => {
         const { index } = event.detail;
         document.querySelector('.lg-backdrop').style.backgroundColor =
             colours[index];
@@ -410,7 +410,7 @@ jQuery('#select-easing').on('change', function () {
 let methodsInstance;
 const $lgGalleryMethodsDemo = document.getElementById('gallery-methods-demo');
 if ($lgGalleryMethodsDemo) {
-    $lgGalleryMethodsDemo.addEventListener('init', () => {
+    $lgGalleryMethodsDemo.addEventListener('lgInit', () => {
         const previousBtn =
             '<button type="button" aria-label="Previous slide" class="lg-prev"> Prev Slide </button>';
         const nextBtn =
@@ -452,7 +452,7 @@ const $lgDemoUpdateSlides = jQuery('#gallery-update-slides-demo');
 const lgDemoUpdateSlides = $lgDemoUpdateSlides.get(0);
 let slidesUpdated = false;
 if (lgDemoUpdateSlides) {
-    lgDemoUpdateSlides.addEventListener('afterClose', (event) => {
+    lgDemoUpdateSlides.addEventListener('lgAfterClose', (event) => {
         if (slidesUpdated) {
             setTimeout(() => {
                 $lgDemoUpdateSlides.justifiedGallery('destroy');
@@ -494,7 +494,7 @@ if (lgDemoUpdateSlides) {
             }, 200);
         }
     });
-    lgDemoUpdateSlides.addEventListener('init', (event) => {
+    lgDemoUpdateSlides.addEventListener('lgInit', (event) => {
         let updateSlideInstance = event.detail.instance;
         const addBtn =
             '<button type="button" aria-label="Add slide" class="lg-icon" id="lg-add"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23 12c0-3.037-1.232-5.789-3.222-7.778s-4.741-3.222-7.778-3.222-5.789 1.232-7.778 3.222-3.222 4.741-3.222 7.778 1.232 5.789 3.222 7.778 4.741 3.222 7.778 3.222 5.789-1.232 7.778-3.222 3.222-4.741 3.222-7.778zM21 12c0 2.486-1.006 4.734-2.636 6.364s-3.878 2.636-6.364 2.636-4.734-1.006-6.364-2.636-2.636-3.878-2.636-6.364 1.006-4.734 2.636-6.364 3.878-2.636 6.364-2.636 4.734 1.006 6.364 2.636 2.636 3.878 2.636 6.364zM8 13h3v3c0 0.552 0.448 1 1 1s1-0.448 1-1v-3h3c0.552 0 1-0.448 1-1s-0.448-1-1-1h-3v-3c0-0.552-0.448-1-1-1s-1 0.448-1 1v3h-3c-0.552 0-1 0.448-1 1s0.448 1 1 1z"></path></svg></button>';
