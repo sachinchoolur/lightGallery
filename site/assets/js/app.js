cf4c5a5dbb8c6a45e38c642a6057bcffdc1d3e8f
Sachin Neravath <sachi77n@gmail.com>
2021-05-05 12:20:46 +0530

    docs(infinite-scrolling): add infinite scrolling demo

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -931,3 +931,66 @@ lightGallery(document.getElementById('gallery-demo-super-customizable'), {
         rotate: false,
     },
 });
+
+const $infiniteScrollingGallery = $('#infinite-scroll-gallery');
+if ($('#infinite-scroll-gallery').length) {
+    let infiniteScrollingGallery = lightGallery(
+        $infiniteScrollingGallery[0],
+        {},
+    );
+
+    const images = `
+    <a data-lg-size="1600-1067" data-pinterest-text="Pin it3" data-tweet-text="share on twitter 4"
+            class="gallery-item"
+            data-src="https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80"
+            data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@flovayn' >Jay Mantri</a></h4><p>  Misty shroud over a forest</p>">
+            <img class="img-responsive"
+                src="https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80" />
+        </a>
+        <a data-lg-size="1600-1067" data-pinterest-text="Pin it3" data-tweet-text="share on twitter 4"
+            class="gallery-item"
+            data-src="https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80"
+            data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@flovayn' >Florian van Duyn</a></h4><p>Location - <a href='Bled, Slovenia'>Bled, Slovenia</a> </p>">
+            <img class="img-responsive"
+                src="https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80" />
+        </a>
+        <a data-lg-size="1600-1126" data-pinterest-text="Pin it3" data-tweet-text="share on twitter 4"
+            class="gallery-item"
+            data-src="https://images.unsplash.com/photo-1477322524744-0eece9e79640?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80"
+            data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@juanster' >Juan Davila</a></h4><p>Location - <a href='Bled, Slovenia'>Bled, Slovenia</a> Wooded lake island </p>">
+            <img class="img-responsive"
+                src="https://images.unsplash.com/photo-1477322524744-0eece9e79640?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80" />
+        </a>
+        <a data-lg-size="1600-1063" data-pinterest-text="Pin it3" data-tweet-text="share on twitter 4"
+            class="gallery-item"
+            data-src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80"
+            data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@davidmarcu' >David Marcu</a></h4><p>Location - <a href='https://unsplash.com/s/photos/ciuca%C8%99-peak%2C-romania'>Ciucaș Peak, Romania</a> Alone in the unspoilt wilderness </p>">
+            <img class="img-responsive"
+                src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80" />
+        </a>`;
+
+    let shouldReInit = true;
+    $(window).on('scroll', function () {
+        if (
+            $(window).scrollTop() >=
+            $(document).height() -
+                $(window).height() -
+                ($(document).height() -
+                    ($infiniteScrollingGallery.position().top +
+                        $infiniteScrollingGallery.outerHeight(true)))
+        ) {
+            if (shouldReInit) {
+                shouldReInit = false;
+                $('#infinite-scroll-gallery').append(images);
+                infiniteScrollingGallery.destroy();
+                setTimeout(() => {
+                    infiniteScrollingGallery = lightGallery(
+                        $infiniteScrollingGallery[0],
+                        {},
+                    );
+                    shouldReInit = true;
+                }, 500);
+            }
+        }
+    });
+}
