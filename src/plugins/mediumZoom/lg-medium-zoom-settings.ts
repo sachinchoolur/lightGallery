4e993a39b6317354a4cc788617634434f6ac7eee
Sachin Neravath <sachi77n@gmail.com>
2021-05-23 13:42:46 +0530

    feat(medium-zoom): allow option to pass background color for each image

--- src/plugins/mediumZoom/lg-medium-zoom-settings.ts
+++ src/plugins/mediumZoom/lg-medium-zoom-settings.ts
@@ -8,9 +8,16 @@ export interface MediumZoomSettings {
      * Space between the gallery outer area and images
      */
     margin: number;
+
+    /**
+     * Background color for the gallery
+     * This can be overwritten by passing background color via `lg-background-color` for each item
+     */
+    backgroundColor: string;
 }
 
 export const mediumZoomSettings: MediumZoomSettings = {
     margin: 40,
     mediumZoom: true,
+    backgroundColor: '#000',
 };
