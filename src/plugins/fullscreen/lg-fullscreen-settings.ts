e652e4124b34e1f5b8f93ac512d1768f03ad38aa
Arnaud Ober <arnaudober@oliver.agency>
2021-09-30 10:14:19 -0400

    feat: add strings on fullscreen plugin for translation

--- src/plugins/fullscreen/lg-fullscreen-settings.ts
+++ src/plugins/fullscreen/lg-fullscreen-settings.ts
@@ -3,8 +3,11 @@ export interface FullscreenSettings {
      * Enable/Disable fullscreen option
      */
     fullScreen: boolean;
+
+    fullscreenPluginStrings: { [key: string]: string };
 }
 
 export const fullscreenSettings: FullscreenSettings = {
     fullScreen: true,
+    fullscreenPluginStrings: { toggleFullscreen: 'Toggle Fullscreen' },
 };
