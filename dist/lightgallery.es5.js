b1caa4abf302cf512eab6e2565ce430ad3dafc7d
Sachin Neravath <sachi77n@gmail.com>
2022-08-03 16:48:04 +0530

    build: version 2.6.0-beta.2 release

--- dist/lightgallery.es5.js
+++ dist/lightgallery.es5.js
@@ -1,5 +1,5 @@
 /*!
- * lightgallery | 2.6.0-beta.1 | July 13th 2022
+ * lightgallery | 2.6.0-beta.2 | August 3rd 2022
  * http://www.lightgalleryjs.com/
  * Copyright (c) 2020 Sachin Neravath;
  * @license GPLv3
@@ -2687,6 +2687,15 @@ var LightGallery = /** @class */ (function () {
         this.updateCounterTotal();
         this.manageSingleSlideClassName();
     };
+    LightGallery.prototype.destroyGallery = function () {
+        this.destroyModules(true);
+        if (!this.settings.dynamic) {
+            this.invalidateItems();
+        }
+        $LG(window).off(".lg.global" + this.lgId);
+        this.LGel.off('.lg');
+        this.$container.remove();
+    };
     /**
      * Destroy lightGallery.
      * Destroy lightGallery and its plugin instances completely
@@ -2701,17 +2710,13 @@ var LightGallery = /** @class */ (function () {
      *
      */
     LightGallery.prototype.destroy = function () {
-        var _this = this;
         var closeTimeout = this.closeGallery(true);
-        setTimeout(function () {
-            _this.destroyModules(true);
-            if (!_this.settings.dynamic) {
-                _this.invalidateItems();
-            }
-            $LG(window).off(".lg.global" + _this.lgId);
-            _this.LGel.off('.lg');
-            _this.$container.remove();
-        }, closeTimeout);
+        if (closeTimeout) {
+            setTimeout(this.destroyGallery.bind(this), closeTimeout);
+        }
+        else {
+            this.destroyGallery();
+        }
         return closeTimeout;
     };
     return LightGallery;
