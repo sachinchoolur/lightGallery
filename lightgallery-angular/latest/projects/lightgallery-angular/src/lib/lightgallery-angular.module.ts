4dbba40a805a26e77805d77aaa0a6e6af80ce055
Sachin Neravath <sachi77n@gmail.com>
2022-09-19 13:41:18 +0530

    refactor(angular): remove latest folder for angular

--- lightgallery-angular/latest/projects/lightgallery-angular/src/lib/lightgallery-angular.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { LightgalleryComponent } from './lightgallery-angular.component';
-
-@NgModule({
-    declarations: [LightgalleryComponent],
-    imports: [],
-    exports: [LightgalleryComponent],
-})
-export class LightgalleryModule {}
--- lightgallery-angular/latest/projects/lightgallery-angular/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of lightgallery-angular
- */
-
-export * from './lib/lightgallery-angular.service';
-export * from './lib/lightgallery-angular.component';
-export * from './lib/lightgallery-angular.module';
