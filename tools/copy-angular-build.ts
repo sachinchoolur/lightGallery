bfb1dbffe3768a4880d5c1149f2568b4d60e9256
Sachin Neravath <sachi77n@gmail.com>
2022-09-19 13:45:07 +0530

    build: add scripts to build all angular components

--- tools/copy-angular-build.ts
+++ tools/copy-angular-build.ts
@@ -2,9 +2,15 @@ const fs = require('fs-extra');
 const colors = require('colors/safe');
 
 (async function main() {
-    fs.copySync('lightgallery-angular/latest/dist', 'dist/angular');
+    fs.copySync('lightgallery-angular/14/dist', 'dist/angular');
     fs.moveSync('dist/angular/lightgallery-angular', 'dist/angular');
     console.log(colors.green('Copied latest angular dist!'));
+    fs.copySync('lightgallery-angular/13/dist', 'dist/angular/13');
+    fs.moveSync('dist/angular/13/lightgallery-angular', 'dist/angular/13');
+    console.log(colors.green('Copies angular 13 dist!'));
+    fs.copySync('lightgallery-angular/12/dist', 'dist/angular/12');
+    fs.moveSync('dist/angular/12/lightgallery-angular', 'dist/angular/12');
+    console.log(colors.green('Copies angular 12 dist!'));
     fs.copySync('lightgallery-angular/11/dist', 'dist/angular/11');
     fs.moveSync('dist/angular/11/lightgallery-angular', 'dist/angular/11');
     console.log(colors.green('Copies angular 11 dist!'));
